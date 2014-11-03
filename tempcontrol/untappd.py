2fd5a6ea85530903519dc29756be8e5b73ce4f62
Adrien Magnus <adrien.magnus@gmail.com>
2014-11-02 16:39:59 -0800

    Adding sensor polling

--- tempcontrol/untappd.py
+++ tempcontrol/untappd.py
@@ -4,12 +4,22 @@ from django.conf import settings
 
 endpoint = 'https://api.untappd.com/v4'
 
+credentials = '?client_id=%s&client_secret=%s&compact=true' % (settings.UNTAPPD_CLIENT_ID,
+    settings.UNTAPPD_CLIENT_SECRET)
+
+
 def pull_beer():
-    path = '%s/beer/info/3558?client_id=%s&client_secret=%s&compact=true' % (endpoint, settings.UNTAPPD_CLIENT_ID, settings.UNTAPPD_CLIENT_SECRET)
+    path = '%s/beer/info/3558%s' % (endpoint, credentials)
 
     r = requests.get(path)
     print r.text
 
 
+def beer_search():
+    path = '%s/search/beer%s&q=moinette' % (endpoint, credentials)
 
-pull_beer()
+    r = requests.get(path)
+    print r.text
+
+
+beer_search()
