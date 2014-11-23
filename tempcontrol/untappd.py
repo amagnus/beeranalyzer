131518c19e367930e9cab6fed4919ea61fd7263d
Adrien Magnus <adrien.magnus@gmail.com>
2014-11-22 17:32:05 -0800

    Edit requirements

--- tempcontrol/untappd.py
+++ tempcontrol/untappd.py
@@ -1,11 +1,14 @@
 import requests
 from django.conf import settings
+import os
 
 
 endpoint = 'https://api.untappd.com/v4'
 
-credentials = '?client_id=%s&client_secret=%s&compact=true' % (settings.UNTAPPD_CLIENT_ID,
-    settings.UNTAPPD_CLIENT_SECRET)
+#credentials = '?client_id=%s&client_secret=%s&compact=true' % (settings.UNTAPPD_CLIENT_ID,
+#    settings.UNTAPPD_CLIENT_SECRET)
+credentials = '?client_id=%s&client_secret=%s&compact=true' % (os.environ['UNTAPPD_CLIENT_ID'],
+    os.environ['UNTAPPD_CLIENT_SECRET'])
 
 
 def pull_beer():
